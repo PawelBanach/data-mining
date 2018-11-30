7705781f68217ae33410167a2bd51f8cb43c7692
Paul Banach <pawel.banach@chargify.com>
2018-11-30 16:51:40 +0100

    Parse wholde dblp set

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Data Mining - Detecting the community in the DBLP co-authorship graph
+
+1. Copy `dblp.xml` set to `/scprits` folder.
+2. Run `parse_data.py`
--- scripts/parse_data.py
+++ scripts/parse_data.py
@@ -6,11 +6,13 @@ from igraph import Graph, mean
 from itertools import combinations
 import copy
 
-xml = 'displays.xml'
-# xml = 'dblp.xml'
+xml = 'dblp.xml'
+#xml = 'displays.xml'
+
 index = 0
 authors_list = set()
 graph_edges = []
+title = ''
 
 
 def add_authors_to_list(new_authors):
@@ -24,18 +26,37 @@ def add_edges_to_list(new_edges):
 
 
 authors = []
-for event, elem in etree.iterparse(source=xml, dtd_validation=True,
-                                   load_dtd=True):
+i = 0
+max_authors = 0
+for event, elem in etree.iterparse(source=xml, dtd_validation=False,
+                                   load_dtd=True):  # ET.iterparse(xml, events=('start', 'end', 'start-ns', 'end-ns')):
+    if i % 20000 == 0:
+        print(i, len(graph_edges), max_authors)
+        max_authors = 0
+
     if event == 'end':
-        if elem.tag == 'article':
-            if len(authors) > 0:
-                add_authors_to_list(authors)
-                edges = combinations(authors, 2)
-                add_edges_to_list(edges)
+        i = i + 1
+
+        if elem.tag == 'title':
+            title = elem.text
+
+        if elem.tag == 'article' or elem.tag == 'inproceedings' or elem.tag == 'incollection' or elem.tag == 'proceedings' or elem.tag == 'www' or elem.tag == 'phdthesis' or elem.tag == 'mastersthesis' or elem.tag == 'book':
+            if len(authors) > 1:
+                if elem.tag == 'article' or elem.tag == 'inproceedings' or elem.tag == 'incollection' or elem.tag == 'proceedings':
+                    if len(authors) > max_authors:
+                        max_authors = len(authors)
+                    if len(authors) > 200:
+                        print(len(authors), title)
+                    add_authors_to_list(authors)
+                    edges = combinations(authors, 2)
+                    add_edges_to_list(edges)
+                else:
+                    authors = []
             authors = []
         if elem.tag == 'author' and elem.tag is not None:
-            authors.append(elem.text.encode('utf-8'))
-        elem.clear()
+            authors.append(elem.text)
+
+    elem.clear()
 
 g = igraph.Graph()
 
@@ -50,6 +71,25 @@ print('Communities:')
 p = g.community_multilevel()
 print(p)
 
+# print("Components")
+# d = g.components('WEAK')
+# a = d.giant()
+# print()
+#
+#
+# print("Decompose")
+# d = g.decompose('WEAK', 122222222, 7)
+#
+# print(d)
+
+print("Degree distribution")
+i = igraph.Graph.degree_distribution(g)
+print(i)
+
+print("Average clustering coefficient")
+i = igraph.GraphBase.transitivity_avglocal_undirected(g)
+print(i)
+
 print("Vertices")
 i = g.vcount()
 print(i)
@@ -61,14 +101,6 @@ print(i)
 print('Modularity')
 q = g.modularity(p)
 print(q)
-
-print("Average clustering coefficient")
-i = igraph.GraphBase.transitivity_avglocal_undirected(g)
-print(i)
-
-print("Degree distribution")
-i = igraph.Graph.degree_distribution(g)
-print(i)
 
 print("Average degree distribution")
 i = mean(g.degree())
@@ -94,12 +126,12 @@ i = mean(g.eigenvector_centrality())  # look at a combination of a nodes edges a
 # cares if you are a hub, but it also cares how many hubs you are connected to
 print(i)
 
-print("Betweenness")
-between = mean(g.betweenness())  # looks at all the shortest paths that pass through a particular node
-print(between)
+# print("Betweenness")
+# between = mean(g.betweenness())  # looks at all the shortest paths that pass through a particular node
+# print(between)
 
 g.vs["label"] = g.vs["name"]
 
 # layout = g.layout("mds")
 layout = g.layout("graphopt")
-igraph.plot(p, layout=layout, bbox=(10000, 10000), vertex_size=40, edge_width=1, label_size=10)
+igraph.plot(g, layout=layout, bbox=(10000, 10000), vertex_size=40, edge_width=1, label_size=10)
